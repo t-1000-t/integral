f50673fa4944ca86712c83ea39ef237ccaff1fdd
t-1000-t <goognya@gmail.com>
2020-04-01 23:45:35 +0300

    cursor leave

--- src/components/Page/IntegralPage/IntegralPage.jsx
+++ src/components/Page/IntegralPage/IntegralPage.jsx
@@ -9,16 +9,82 @@ import stylish from "./IntegralPage.module.css";
 class IntegralPage extends Component {
   state = {
     arrCategory: [
-      { id: "002", name: "Планшеты" },
-      { id: "001", name: "Ноутбуки" },
-      { id: "003", name: "Компьютеры" },
-      { id: "004", name: "Комплектующие" },
-      { id: "005", name: "Смартфоны" },
-      { id: "006", name: "Принтеры" },
-      { id: "007", name: "Сетевое оборудование" },
-      { id: "008", name: "Телевизоры и проекторы" },
-      { id: "009", name: "Наушники" },
-      { id: "010", name: "Бытовая техника" }
+      {
+        id: "001",
+        name1: "Ноутбуки",
+        category1: "1191",
+        name2: "Аксессуары для ноутбуков",
+        category2: "1211",
+        name3: "Комплектующие к ноутбукам",
+        category3: "1233",
+        name4: "Запчасти для ноутбуков",
+        category4: "8568",
+        name5: "Сумки к ноутбукам",
+        category5: "1204",
+        name6: "Рюкзаки к ноутбукам",
+        category6: "1205",
+        name7: "Подставки для ноутбуков",
+        category7: "1471",
+        name8: "Матриця до ноутбука",
+        category8: "8516",
+        name9: "Модули памяти к ноутбукам",
+        category9: "1237",
+        name10: "Ноутбуки, планшеты",
+        category10: "1181"
+      },
+      {
+        id: "002",
+        name1: "Планшеты",
+        category1: "1192",
+        name2: "Аксессуары для планшетов",
+        category2: "7981",
+        name3: "Чехлы к планшетам",
+        category3: "1207",
+        name4: "Электронные книги",
+        category4: "1194",
+        name5: "Ноутбуки, планшеты",
+        category5: "1181",
+        name6: "",
+        category6: "",
+        name7: "",
+        category7: "",
+        name8: "",
+        category8: "",
+        name9: "",
+        category9: "",
+        name10: "",
+        category10: ""
+      },
+      {
+        id: "003",
+        name1: "Компьютеры",
+        category1: "1053",
+        name2: "Компьютеры, аксессуары",
+        category2: "1331",
+        name3: "Модули памяти для компьютера",
+        category3: "1334",
+        name4: "Компьютерные игры",
+        category4: "1418",
+        name5: "Компьютерные игры Доп. раздел",
+        category5: "8366",
+        name6: "",
+        category6: "",
+        name7: "",
+        category7: "",
+        name8: "",
+        category8: "",
+        name9: "",
+        category9: "",
+        name10: "",
+        category10: ""
+      },
+      { id: "004", name1: "Комплектующие" },
+      { id: "005", name1: "Смартфоны" },
+      { id: "006", name1: "Принтеры" },
+      { id: "007", name1: "Сетевое оборудование" },
+      { id: "008", name1: "Телевизоры и проекторы" },
+      { id: "009", name1: "Наушники" },
+      { id: "010", name1: "Бытовая техника" }
     ],
     isOpenArrCategory: false,
     isOpenNameCategory: null
@@ -109,7 +175,7 @@ class IntegralPage extends Component {
                       <li className={stylish.liList} key={el.id}>
                         <IntegralPageCategory
                           id={el.id}
-                          name={el.name}
+                          elem={el}
                           liHandlerCategoryTrue={this.liHandlerCategoryTrue}
                           className={stylish.liNameCategory}
                         />
--- src/components/Page/IntegralPage/IntegralPageCategory/IntegralPageCategory.jsx
+++ src/components/Page/IntegralPage/IntegralPageCategory/IntegralPageCategory.jsx
@@ -1,32 +1,56 @@
 import React, { Component } from "react";
 
 import ModalLicategory from "../../../Modals/ModalLicategory/ModalLicategory";
-// import stylish from "./IntegralPageCategory.module.css";
+import stylish from "./IntegralPageCategory.module.css";
 
 class IntegralPageCategory extends Component {
   state = {
     isOpen: false
   };
 
-  handleOnModal = () => {
-    const { isOpen } = this.state;
+  handleOnModalTrue = () => {
     this.setState({
-      isOpen: !isOpen
+      isOpen: true
+    });
+  };
+
+  handleOnModalFalse = () => {
+    this.setState({
+      isOpen: false
     });
   };
 
   render() {
-    const { id, name } = this.props;
+    const { id, elem } = this.props;
     return (
       <>
         <div
+          className={stylish.cellCatagory}
           id={id}
-          onMouseEnter={this.handleOnModal}
-          ref={this.categoryNameRef}
+          onClick={this.handleOnModalTrue}
         >
-          {name}
+          <span> {elem.name1}</span>
         </div>
-        {this.state.isOpen && <ModalLicategory id={id}>{name}</ModalLicategory>}
+        {this.state.isOpen && (
+          <ModalLicategory id={id}>
+            <div
+              className={stylish.boxName}
+              onMouseEnter={this.handleOnModalTrue}
+              onMouseLeave={this.handleOnModalFalse}
+            >
+              <div className={stylish.boxCell}>{elem.name1}</div>
+              <div className={stylish.boxCell}>{elem.name2}</div>
+              <div className={stylish.boxCell}>{elem.name3}</div>
+              <div className={stylish.boxCell}>{elem.name4}</div>
+              <div className={stylish.boxCell}>{elem.name5}</div>
+              <div className={stylish.boxCell}>{elem.name6}</div>
+              <div className={stylish.boxCell}>{elem.name7}</div>
+              <div className={stylish.boxCell}>{elem.name8}</div>
+              <div className={stylish.boxCell}>{elem.name9}</div>
+              <div className={stylish.boxCell}>{elem.name10}</div>
+            </div>
+          </ModalLicategory>
+        )}
       </>
     );
   }
